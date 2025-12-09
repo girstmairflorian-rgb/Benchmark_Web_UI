c66a4f6b816a31fca99949168626285dcfaa4a88
girstmairflorian-rgb <girstmairflorian@gmail.com>
2025-12-09 12:59:06 +0100

    Implement routes

--- app.py
+++ app.py
@@ -2,8 +2,23 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
-    return "Placeholder"
+    return render_template("index.html")
 
-app.run(debug=True)
+@app.route("/results", methods=["POST"])
+def results():
+    algorithm = request.form["algorithm"]
+    benchmark = request.form["benchmark"]
+    upper_limit = int(request.form["upperLimit"])
+    num_processes = int(request.form["numProcesses"])
+
+    return (
+        f"Algorithm: {algorithm}<br>"
+        f"Benchmark: {benchmark}<br>"
+        f"Upper Limit: {upper_limit}<br>"
+        f"Processes: {num_processes}<br>"
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
