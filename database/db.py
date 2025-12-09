742afa97bab57124440a661fc6cd4347e7bf2742
girstmairflorian-rgb <girstmairflorian@gmail.com>
2025-12-09 20:17:59 +0100

    Refactor db.py using context managers

--- database/db.py
+++ database/db.py
@@ -5,32 +5,24 @@ DB_PATH = Path("database/results.db")
 DB_PATH.parent.mkdir(exist_ok=True)
 
 def init_db():
-    conn = sqlite3.connect(DB_PATH)
-    cur = conn.cursor()
-
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS benchmark_results (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            algorithm TEXT,
-            benchmark_method TEXT,
-            upper_limit INTEGER,
-            num_processes INTEGER,
-            execution_time REAL
-        );
-    """)
-
-    conn.commit()
-    conn.close()
+    with sqlite3.connect(DB_PATH) as conn:
+        cur = conn.cursor()
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS benchmark_results(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                algorithm TEXT,
+                benchmark_method TEXT,
+                upper_limit INTEGER,
+                num_processes INTEGER,
+                execution_time REAL
+            );
+        """)
 
 def save_result(algorithm, benchmark, upper_limit, num_processes, execution_time):
-    conn = sqlite3.connect(DB_PATH)
-    cur = conn.cursor()
-
-    cur.execute("""
-        INSERT INTO benchmark_results
-        (algorithm, benchmark_method, upper_limit, num_processes, execution_time)
-        VALUES (?, ?, ?, ?, ?);
-    """, (algorithm, benchmark, upper_limit, num_processes, execution_time))
-
-    conn.commit()
-    conn.close()
+    with sqlite3.connect(DB_PATH) as conn:
+        cur = conn.cursor()
+        cur.execute("""
+                    INSERT INTO benchmark_results
+                    (algorithm, benchmark_method, upper_limit, num_processes, execution_time)
+                    VALUES (?, ?, ?, ?, ?);
+                    """, (algorithm, benchmark, upper_limit, num_processes, execution_time))
